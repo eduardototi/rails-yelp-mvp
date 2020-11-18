202b179513ce736b23b6a3f82219d8ceeab6c5f1
Pedro Eduardo <eduardototi@gmail.com>
2020-11-18 18:02:17 -0300

    28 day challenge 2

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  RATING = [0, 1, 2, 3, 4, 5]
+  belongs_to :restaurant
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: 0..5}
+  validates :content, presence: true
+end
